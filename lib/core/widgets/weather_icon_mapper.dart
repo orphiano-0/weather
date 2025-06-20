5f7e075cbfd2fb25c6a98ba7671c92998a4cc6cb
Gabriel Orphiano <gab.orphiano@gmail.com>
2025-06-21 00:01:47 +0800

    Finished Consuming Web Services Activity

--- lib/core/widgets/weather_icon_mapper.dart
+++ lib/core/widgets/weather_icon_mapper.dart
@@ -1,20 +1,23 @@
-String getWeatherIcon(String weatherMain) {
-  switch (weatherMain.toLowerCase()) {
-    case 'clear':
-      return 'assets/images/clear.png';
-    case 'clouds':
-      return 'assets/images/cloudy.png';
-    case 'rain':
-      return 'assets/images/rain.png';
-    case 'drizzle':
-      return 'assets/images/drizzle.png';
-    case 'thunderstorm':
-      return 'assets/images/thunderstorm.png';
-    case 'mist':
-    case 'fog':
-    case 'haze':
-      return 'assets/images/mist.png';
-    default:
-      return 'assets/images/clear.png'; //fallback
+String getWeatherIcon(String weatherDescription) {
+  final description = weatherDescription.toLowerCase();
+
+  if (description.contains('clear')) {
+    return 'assets/images/clear.png';
+  } else if (description.contains('cloud')) {
+    return 'assets/images/cloudy.png';
+  } else if (description.contains('rain')) {
+    return 'assets/images/rain.png';
+  } else if (description.contains('drizzle')) {
+    return 'assets/images/drizzle.png';
+  } else if (description.contains('thunderstorm')) {
+    return 'assets/images/thunderstorm.png';
+  } else if (description.contains('mist') ||
+      description.contains('fog') ||
+      description.contains('haze')) {
+    return 'assets/images/mist.png';
+  } else if (description.contains('snow')) {
+    return 'assets/images/snow.png';
+  } else {
+    return 'assets/images/clear.png'; // fallback
   }
 }
